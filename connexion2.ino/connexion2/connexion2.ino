5419efe9197de794d6a7734774199d8656abc09a
leoESILV <leonard92.j@gmail.com>
2024-10-11 18:53:35 +0200

    version final - JS non fonctionnel

--- connexion2.ino/connexion2/connexion2.ino
+++ connexion2.ino/connexion2/connexion2.ino
@@ -2,70 +2,63 @@
 //WIFI : PoleDeVinci_IFT MDP: *c.r4UV@VfPn_0
 // https://github.com/leoESILV/CS_workshop_octobre/blob/main/index.html
 //http://127.0.0.1:5500/index.html
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+//Adresse IP : 10.1.224.40
+// DC Arduino controle - l293d
 
-// Informations de connexion Wi-Fi
-const char* ssid = "PoleDeVinci_IFT";        
-const char* password = "*c.r4UV@VfPn_0";    
+#include <ESP8266WiFi.h>  // Utilisé pour l'ESP8266
+#include <WebSocketsServer.h>
 
-// connexion au serveur 
-const char* host = "http://127.0.0.1:5500/index.html";  
+const char* ssid = "PoleDeVinci_IFT";
+const char* password = "*c.r4UV@VfPn_0";
 
-// Délai maximum pour se connecter au Wi-Fi (en millisecondes)
-unsigned long wifiTimeout = 10000;  // 10 secondes
+
+//pour allumer la led 
+int ledPin = 16 ;
+
+WebSocketsServer webSocket = WebSocketsServer(80); // port 80 ( port html)
+
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  // Handle WebSocket events here
+  if (type == WStype_TEXT) {
+      // Message reçu
+      String message = (char *)payload;
+      // Traitement du message
+      Serial.println("Message reçu : " + message);
+      // Send a response back to the client
+      webSocket.sendTXT(num, "Message reçu et traité !");
+    }
+  
+}
 
 void setup() {
+
+   // initialisation de la sortie pour la led 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  //initilasioton wifi
   Serial.begin(115200);
-
-  // Connexion au Wi-Fi
-  Serial.println();
-  Serial.println("Connexion au Wi-Fi...");
-  
   WiFi.begin(ssid, password);
-
-  unsigned long startAttemptTime = millis();  // Enregistre le temps de début de la tentative
-
-  // Attendre la connexion au Wi-Fi avec timeout
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < wifiTimeout) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(".");
+    Serial.println("Connecting to WiFi...");
   }
+  Serial.println("Connected to :");
+  Serial.println(ssid) ;
 
-  // Vérifier si la connexion a échoué
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("");
-    Serial.println("Erreur : Impossible de se connecter au Wi-Fi.");
-    return;  // Arrête le reste de la fonction setup si pas connecté
-  }
+// voir adresse IP :
+ Serial.print("Utiliser cette adresse URL pour la connexion :");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+  //web socket
+  Serial.println("Starting WebSocket server...");
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
 
-  // Si connecté, afficher les informations
-  Serial.println("");
-  Serial.println("Connecté au Wi-Fi !");
-  Serial.print("Adresse IP : ");
-  Serial.println(WiFi.localIP());  // Affiche l'adresse IP locale
 
-  // Connecter l'ESP8266 au serveur GitHub Pages
-  WiFiClient client;
-  if (!client.connect(host, 80)) {  // Port 80 pour HTTP
-    Serial.println("Échec de la connexion au serveur");
-    return;
-  }
-
-  // Faire une requête HTTP GET à GitHub Pages pour récupérer la page index.html
-  client.print(String("GET /mon-site/index.html HTTP/1.1\r\n") + 
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-
-  // Attendre la réponse du serveur
-  while (client.connected() || client.available()) {
-    if (client.available()) {
-      String line = client.readStringUntil('\n');
-      Serial.println(line);  // Affiche la réponse du serveur dans le Moniteur Série
-    }
-  }
 }
 
 void loop() {
-  // Rien dans la boucle, car tout est fait dans le setup pour cet exemple
+  webSocket.loop();  // Gérer les clients WebSocket
 }
--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 
 
-const test = 1;
-let test2 = 2;
-var test3 = 3;
+//const test = 1;
+//let test2 = 2;
+//var test3 = 3;
 
 
+var ws;
+var motorState = false;
+var ledState = false;
+
+function initWebSocket() {
+    ws = new WebSocket('ws://10.1.224.40');
+
+    ws.onopen = function () {
+        document.getElementById('status').innerHTML = "WebSocket est connecté";
+    };
+
+    ws.onmessage = function (event) {
+        console.log(event.data);
+        if (event.data.includes("temp:")) {
+            document.getElementById('temperature').innerText = event.data.split(":")[1] + "°";
+        }
+    };
+
+    ws.onclose = function () {
+        document.getElementById('status').innerHTML = "WebSocket déconnecté";
+    };
+}
+
+document.getElementById('motorButton').addEventListener('click', function () {
+    motorState = !motorState;
+    ws.send(motorState ? 'MOTOR_ON' : 'MOTOR_OFF');
+    document.getElementById('motorButton').innerText = motorState ? 'OFF' : 'ON';
+});
+
+document.getElementById('ledButton').addEventListener('click', function () {
+    ledState = !ledState;
+    ws.send(ledState ? 'LED_ON' : 'LED_OFF');
+    document.getElementById('ledButton').innerText = ledState ? 'OFF' : 'ON';
+});
+
+window.onload = function () {
+    initWebSocket();
+};
